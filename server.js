0432baca8bf8187c954e7b2c99ad486f750ac8ca
Kelvin Hu <kha115@sfu.ca>
2023-11-13 23:33:24 -0800

    Implemented get-categories and get-question WebSocket events

--- server.js
+++ server.js
@@ -28,8 +28,32 @@ fastify.get("/game", async function(request, response) {
 fastify.get("/api/v1/ws/game", {
     websocket: true
 }, async function(ws_connection, request) {
-    ws_connection.socket.on("get-question", function(message) {
+    ws_connection.socket.on("get-categories", async function(message) {
+        let categories = await fetch("https://opentdb.com/api_category.php");
+        categories = await categories.json();
 
+        ws_connection.socket.send(categories);
+    });
+
+    ws_connection.socket.on("get-question", async function(message) {
+        const question_parameters = JSON.parse(message.to_string());
+
+        const category = question_parameters.category;
+        const amount = question_parameters.number_of_questions;
+
+        let url = `https://opentdb.com/api.php?category=${category}&amount=${amount}`;
+
+        if(question_parameters.diffculty !== "any") {
+            url.concat(`&difficulty=${question_parameters.diffculty}`);
+        }
+
+        if(question_parameters.type !== "any") {
+            url.concat(`&type=${question_parameters.type}`);
+        }
+
+        let questions = await fetch(url);
+        questions = await questions.json();
+        ws_connection.socket.send(questions); 
     });
 });
 
