098b52b628d9c2a6da830f604290967628efbd1b
Kelvin Hu <kha115@sfu.ca>
2023-11-14 00:23:04 -0800

    Fixed missing Fastify plugin register for WebSocket endpoints

--- server.js
+++ server.js
@@ -25,37 +25,45 @@ fastify.get("/game", async function(request, response) {
 });
 
 // Handle single-player mode 
-fastify.get("/api/v1/ws/game", {
-    websocket: true
-}, async function(ws_connection, request) {
-    ws_connection.socket.on("get-categories", async function(message) {
-        let categories = await fetch("https://opentdb.com/api_category.php");
-        categories = await categories.json();
-
-        ws_connection.socket.send(categories);
+fastify.register(async function(fastify) {
+    fastify.get("/api/v1/ws/game", {
+        websocket: true
+    }, async function(ws_connection, request) {
+        console.log("Client connected");
+    
+        ws_connection.socket.on("get-categories", async function(message) {
+            let categories = await fetch("https://opentdb.com/api_category.php");
+            categories = await categories.json();
+    
+            ws_connection.socket.send(categories);
+        });
+    
+        ws_connection.socket.on("get-question", async function(message) {
+            const question_parameters = JSON.parse(message.to_string());
+    
+            const category = question_parameters.category;
+            const amount = question_parameters.number_of_questions;
+    
+            let url = `https://opentdb.com/api.php?category=${category}&amount=${amount}`;
+    
+            if(question_parameters.diffculty !== "any") {
+                url.concat(`&difficulty=${question_parameters.diffculty}`);
+            }
+    
+            if(question_parameters.type !== "any") {
+                url.concat(`&type=${question_parameters.type}`);
+            }
+    
+            let questions = await fetch(url);
+            questions = await questions.json();
+            ws_connection.socket.send(questions); 
+        });
+    
+        ws_connection.socket.on("close", function(message) {
+            console.log("Client disconnected")
+        })
     });
-
-    ws_connection.socket.on("get-question", async function(message) {
-        const question_parameters = JSON.parse(message.to_string());
-
-        const category = question_parameters.category;
-        const amount = question_parameters.number_of_questions;
-
-        let url = `https://opentdb.com/api.php?category=${category}&amount=${amount}`;
-
-        if(question_parameters.diffculty !== "any") {
-            url.concat(`&difficulty=${question_parameters.diffculty}`);
-        }
-
-        if(question_parameters.type !== "any") {
-            url.concat(`&type=${question_parameters.type}`);
-        }
-
-        let questions = await fetch(url);
-        questions = await questions.json();
-        ws_connection.socket.send(questions); 
-    });
-});
+})
 
 fastify.post("/api/v1/user/register", async function(request, response) {
 
