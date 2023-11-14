b0180f8b9fe20060f538e33bf1c6a2f46726748c
Kelvin Hu <kha115@sfu.ca>
2023-11-14 00:40:41 -0800

    Corrected incorrect usage of custom events in WS handler/router

--- server.js
+++ server.js
@@ -31,33 +31,40 @@ fastify.register(async function(fastify) {
     }, async function(ws_connection, request) {
         console.log("Client connected");
     
-        ws_connection.socket.on("get-categories", async function(message) {
-            let categories = await fetch("https://opentdb.com/api_category.php");
-            categories = await categories.json();
-    
-            ws_connection.socket.send(categories);
-        });
-    
-        ws_connection.socket.on("get-question", async function(message) {
-            const question_parameters = JSON.parse(message.to_string());
-    
-            const category = question_parameters.category;
-            const amount = question_parameters.number_of_questions;
-    
-            let url = `https://opentdb.com/api.php?category=${category}&amount=${amount}`;
-    
-            if(question_parameters.diffculty !== "any") {
-                url.concat(`&difficulty=${question_parameters.diffculty}`);
+        ws_connection.socket.on("message", async function(message) {
+            const client_message = JSON.parse(message.toString());
+            
+            console.log(message.toString());
+
+            switch(client_message.opcode) {
+                case "get-categories":
+                    let categories = await fetch("https://opentdb.com/api_category.php");
+                    categories = await categories.json();
+            
+                    ws_connection.socket.send(JSON.stringify(categories));
+                    break;
+                case "get-question":
+                    const category = question_parameters.category;
+                    const amount = question_parameters.number_of_questions;
+            
+                    let url = `https://opentdb.com/api.php?category=${category}&amount=${amount}`;
+            
+                    if(question_parameters.diffculty !== "any") {
+                        url.concat(`&difficulty=${question_parameters.diffculty}`);
+                    }
+            
+                    if(question_parameters.type !== "any") {
+                        url.concat(`&type=${question_parameters.type}`);
+                    }
+            
+                    let questions = await fetch(url);
+                    questions = await questions.json();
+                    ws_connection.socket.send(JSON.stringify(questions)); 
+                    break;
+                default:
+                    break;
             }
-    
-            if(question_parameters.type !== "any") {
-                url.concat(`&type=${question_parameters.type}`);
-            }
-    
-            let questions = await fetch(url);
-            questions = await questions.json();
-            ws_connection.socket.send(questions); 
-        });
+        })
     
         ws_connection.socket.on("close", function(message) {
             console.log("Client disconnected")
