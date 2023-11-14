0848b647a710ba4fe237b3061369f08c26488080
Kelvin Hu <kha115@sfu.ca>
2023-11-13 20:17:31 -0800

    Added dependency to allow for Fastify integrated WebSocket server

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 "use strict";
 
 import Fastify from "fastify";
+import fastifyWebsocket from "@fastify/websocket";
 
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 8080;
 
 const fastify = Fastify({
     logger: true
-})
+});
 
-// Routings
+// Register websocket plugin
+fastify.register(fastifyWebsocket);
+
+/* 
+    Routings
+*/
 fastify.get("/", async function(request, response) {
-    return {
-        hello: "world"
-    };
+
+});
+
+fastify.get("/game", async function(request, response) {
+
+});
+
+// Handle single-player mode 
+fastify.get("/api/v1/ws/game", {
+    websocket: true
+}, async function(ws_connection, request) {
+    ws_connection.socket.on("get-question", function(message) {
+
+    });
+});
+
+fastify.post("/api/v1/user/register", async function(request, response) {
+
 });
 
 // Start the server
