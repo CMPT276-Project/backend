3b83628f9d0f02bb20046e82d9d97598bf50de39
Kelvin Hu <kha115@sfu.ca>
2023-11-15 16:06:55 -0800

    Revert "Removed unneeded REST endpoints, WebSocket endpoint, and WebSocket plug registration"
    This reverts commit 4d184014a00d51d9b400e7463f23ac23f8743698.

--- server.js
+++ server.js
@@ -10,6 +10,72 @@ const fastify = Fastify({
     logger: true
 });
 
+// Register websocket plugin
+fastify.register(fastifyWebsocket);
+
+/* 
+    Routings
+*/
+fastify.get("/", async function(request, response) {
+
+});
+
+fastify.get("/game", async function(request, response) {
+
+});
+
+// Handle single-player mode 
+fastify.register(async function(fastify) {
+    fastify.get("/api/v1/ws/game", {
+        websocket: true
+    }, async function(ws_connection, request) {
+        console.log("Client connected");
+    
+        ws_connection.socket.on("message", async function(message) {
+            const client_message = JSON.parse(message.toString());
+            
+            console.log(message.toString());
+
+            switch(client_message.opcode) {
+                case "get-categories":
+                    let categories = await fetch("https://opentdb.com/api_category.php");
+                    categories = await categories.json();
+            
+                    ws_connection.socket.send(JSON.stringify(categories));
+                    break;
+                case "get-question":
+                    const category = client_message.payload.category;
+                    const amount = client_message.payload.number_of_questions;
+            
+                    let url = `https://opentdb.com/api.php?category=${category}&amount=${amount}`;
+            
+                    if(client_message.diffculty !== "any") {
+                        url.concat(`&difficulty=${client_message.diffculty}`);
+                    }
+            
+                    if(client_message.type !== "any") {
+                        url.concat(`&type=${client_message.type}`);
+                    }
+
+                    let questions = await fetch(url);
+                    questions = await questions.json();
+                    ws_connection.socket.send(JSON.stringify(questions)); 
+                    break;
+                default:
+                    break;
+            }
+        })
+    
+        ws_connection.socket.on("close", function(message) {
+            console.log("Client disconnected")
+        })
+    });
+})
+
+fastify.post("/api/v1/user/register", async function(request, response) {
+
+});
+
 // Start the server
 const start = async function() {
     try {
