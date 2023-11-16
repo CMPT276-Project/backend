42650de67f8dd2fc384bffe6f144584d15bcaf20
Kelvin Hu <kha115@sfu.ca>
2023-11-15 17:09:43 -0800

    Further removed usage of @fastify/websocket

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 "use strict";
 
 import Fastify from "fastify";
-import fastifyWebsocket from "@fastify/websocket";
 
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 8080;
@@ -9,9 +8,6 @@ const port = process.env.PORT || 8080;
 const fastify = Fastify({
     logger: true
 });
-
-// Register websocket plugin
-fastify.register(fastifyWebsocket);
 
 /* 
     Routings
