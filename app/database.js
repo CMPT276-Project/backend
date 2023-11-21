56f429abb9747d6fb9ac62ce80f2bc621ae17b54
Kelvin Hu <kha115@sfu.ca>
2023-11-21 12:48:21 -0800

    Implemented skeleton and certain table operations

--- app/database.js
+++ app/database.js
@@ -4,7 +4,9 @@ import sqlite from "sqlite3";
 
 class Database {
     /*
-        Database manager (explicitly not a comprehensive ORM) for SQLite.
+        Database connection manager (explicitly not a comprehensive ORM) for SQLite.
+
+        This function only executes commands and passes through results back to the caller.
 
         Arguments:
             db_path (String):   Path to an on-disk SQLite database.
@@ -37,70 +39,45 @@ class Database {
                 VALUES (?, ?);
         `;
         
-        const statement = await this.get_prepared_statement(sql);
-        statement.run(name, points);
+        return this.run_statement(sql, name, points);
     }
 
-    async get_points(name) {
+    async get_difficulty(name) {
         const sql = `
-            SELECT points 
+            SELECT * 
                 FROM difficulties 
                 WHERE difficulty = ?;
         `;
 
-        const statement = await this.get_prepared_statement(sql);
-        
-        return new Promise(function(resolve, reject) {
-            statement.get(name, function(err, row) {
-                if(err != null) {
-                    reject(err);
-                    return;
-                }
-
-                resolve(row);
-            })
-        })
+        return this.get_statement(sql, name);
     }
 
-    async get_difficulties() {
+    async get_all_difficulties() {
         const sql = `
             SELECT * 
                 FROM difficulties;
         `;
         
-        const statement = await this.get_prepared_statement(sql);
-        
-        return new Promise(function(resolve, reject) {
-            statement.all(function(err, rows) {
-                if(err != null) {
-                    reject(err);
-                    return;
-                }
-
-                resolve(rows);
-            })
-        })
+        return this.get_all_statement(sql);
     }
 
-    async update_points(name, points) {
+    async update_difficulty(name, points) {
         const sql = `
             UPDATE difficulties
                 SET points = ?
                 WHERE difficulty = ?;
         `;
 
-        const statement = await this.get_prepared_statement(sql);
-        statement.run(points, name);
+        return this.run_statement(sql, points, name);
     }
 
-    async remove_difficulty(name) {
+    async delete_difficulty(name) {
         const sql = `
             DELETE FROM difficulties 
                 WHERE difficulty = ?;
         `;
 
-        const statement = await this.get_prepared_statement(sql);
-        statement.run(name);
+        return this.run_statement(sql, name);
     }
 
     /*
@@ -109,11 +86,51 @@ class Database {
         =================================================
     */
 
+    async add_category() {
+
+    }
+
+    async get_category() {
+
+    }
+
+    async get_all_categories() {
+
+    }
+
+    async update_category() {
+
+    }
+
+    async remove_category() {
+
+    }
+
     /*
         ==============================================
         CRUD functions related to the 'sources' table.
         ==============================================
     */
+
+    async add_source() {
+
+    }
+
+    async get_source() {
+
+    }
+
+    async get_all_sources() {
+
+    }
+    
+    async update_source() {
+
+    }
+
+    async remove_source() {
+
+    }
 
     /*
         ============================================
@@ -121,17 +138,104 @@ class Database {
         ============================================
     */
 
+    async add_user(guid, name) {
+        const sql = `
+            INSERT INTO users 
+                VALUES(?, ?)
+        `;
+
+        return this.run_statement(sql, guid, name);
+    }
+
+    async get_user(guid) {
+        const sql = `
+            SELECT *
+                FROM users
+                WHERE id = ?;
+        `;
+
+        return this.get_statement(sql, guid);
+    }
+
+    async get_all_users() {
+        const sql = `
+            SELECT *
+                FROM users;
+        `;
+
+        return this.get_all_statement(sql);
+    }
+
+    async update_user(guid, name) {
+        const sql = `
+            UPDATE users
+                SET name = ?
+                WHERE id = ?;
+        `;
+
+        return this.run_statement(sql, name, guid);
+    }
+
+    async remove_user(guid) {
+        const sql = `
+            DELETE FROM users
+                WHERE id = ?;
+        `;
+
+        this.run_statement(sql, guid);
+    }
+
     /*
         =============================================
         CRUD functions related to the 'scores' table.
         =============================================
     */
 
+    async add_score() {
+
+    }
+
+    async get_score() {
+
+    }
+
+    async get_all_scores() {
+
+    }
+
+    async update_score() {
+
+    }
+
+    async remove_score() {
+
+    }
+
     /*
         ================================================
         CRUD functions related to the 'questions' table.
         ================================================
     */
+
+    async add_question() {
+
+    }
+
+    async get_question() {
+
+    }
+
+    async get_all_questions() {
+
+    }
+
+    async update_question() {
+
+    }
+
+    async remove_question() {
+
+    }
 
 
     get_prepared_statement(sql) {
@@ -149,6 +253,50 @@ class Database {
 
             resolve(this.cache[sql]);            
         }.bind(this));
+    }
+
+    run_statement(sql, ...parameters) {
+        const promise = this.get_prepared_statement(sql)
+            .then(function(statement) {
+                statement.run(...parameters, function(err) {
+                    if(err !== null) {
+                        reject(err);
+                        return;
+                    }
+
+                    resolve();
+                });
+            });
+
+        return promise;
+    }
+
+    get_statement(sql, ...parameters) {
+        return this.get_prepared_statement(sql)
+            .then(function(statement) {
+                statement.get(...parameters, function(err, row) {
+                    if(err !== null) {
+                        reject(err, row);
+                        return;
+                    }
+
+                    resolve(row);
+                });
+            });
+    }
+
+    get_all_statement(sql, ...parameters) {
+        return this.get_prepared_statement(sql)
+            .then(function(statement) {
+                statement.all(...parameters, function(err, rows) {
+                    if(err !== null) {
+                        reject(err, rows);
+                        return;
+                    }
+
+                    resolve(rows);
+                });
+            });
     }
 
     close() {
