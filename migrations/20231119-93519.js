bcace7e697169d489775a123c6490d9e1b77ea93
Kelvin Hu <kha115@sfu.ca>
2023-11-23 03:54:36 -0800

    Closes #7, closes #8; massively scaled back database concerns due to time constraints.

--- migrations/20231119-93519.js
+++ migrations/20231119-93519.js
@@ -1,26 +1,5 @@
 db.serialize(function() {
     db.exec(`
-    CREATE TABLE IF NOT EXISTS difficulties(
-        difficulty TEXT,
-        points INTEGER,
-    
-        PRIMARY KEY(difficulty)
-    );
-
-    CREATE TABLE IF NOT EXISTS categories(
-        category TEXT,
-        description TEXT,
-    
-        PRIMARY KEY(category)
-    );
-
-    CREATE TABLE IF NOT EXISTS sources(
-        source_name TEXT,
-        source TEXT,
-    
-        PRIMARY KEY(source_name)
-    );
-
     CREATE TABLE IF NOT EXISTS users(
         id TEXT,
         name TEXT UNIQUE,
@@ -38,32 +17,4 @@ db.serialize(function() {
             REFERENCES users(id)
             ON DELETE CASCADE
     );
-
-    CREATE TABLE IF NOT EXISTS questions(
-        question TEXT,
-        source TEXT DEFAULT "Unknown",
-        difficulty TEXT,
-        category TEXT,
-    
-        PRIMARY KEY(question, source, category),
-    
-        FOREIGN KEY(difficulty)
-            REFERENCES difficulties(difficulty)
-            ON UPDATE CASCADE
-            ON DELETE CASCADE,
-        
-        FOREIGN KEY(category)
-            REFERENCES categories(category)
-            ON UPDATE CASCADE
-            ON DELETE CASCADE,
-    
-        FOREIGN KEY(source)
-            REFERENCES sources(source_name)
-            ON UPDATE CASCADE
-            ON DELETE SET DEFAULT
-    );
-
-    INSERT INTO sources 
-        VALUES("Unknown", "Unknown");
-    `);
 });
