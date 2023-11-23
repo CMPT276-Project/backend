e785a3f73506a01337ba28e2c127685f4b1276e2
Kelvin Hu <kha115@sfu.ca>
2023-11-23 08:12:29 -0800

    Removed tests for no longer existing features

--- tests/database.test.js
+++ tests/database.test.js
@@ -5,18 +5,10 @@ import { Database } from "../app/database";
 const db_path = process.env.DB_PATH || "./testing.sqlite3";
 const db = new Database(db_path);
 
-describe("Table - Difficulties", function() {
-    test("Inserting and retrieving row", async function() {
-        await db.add_difficulty("Testing", 123);
+describe("Users", function() {
 
-        expect(false).toBe(true);
-    });
+})
 
-    test("Retrieving non-existent row", async function() {
-
-    })
-
-    test("Inserting and retrieving row with incorrect data types", async function() {
-
-    })
+describe("Scores", function() {
+    
 })
